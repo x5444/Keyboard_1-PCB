9e0d8ddc02a55d271071030fef45feae5258b338
socke0809 <steffi@dangl-muenchen.de>
2013-10-11 16:18:29 +0200

    Exported rev1 release.

---
+++ build_rev1.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+mkdir RELEASE/rev1
+cp OUTPUT/*.gbl RELEASE/rev1/Keyboard_PCB.GBL
+cp OUTPUT/*.gbo RELEASE/rev1/Keyboard_PCB.GBO
+cp OUTPUT/*.gbs RELEASE/rev1/Keyboard_PCB.GBS
+cp OUTPUT/*.gtl RELEASE/rev1/Keyboard_PCB.GTL
+cp OUTPUT/*.gto RELEASE/rev1/Keyboard_PCB.GTO
+cp OUTPUT/*.gts RELEASE/rev1/Keyboard_PCB.GTS
+cp OUTPUT/*.gto RELEASE/rev1/Keyboard_PCB.GML
+cp OUTPUT/*.drl RELEASE/rev1/Keyboard_PCB.TXT
